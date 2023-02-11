164844166402bc7eb3a9786de76b76bb14d95817
Hoang Long <longnguyen20011123@gmail.com>
2023-02-11 23:25:44 +0700

    update UI

--- Projects/Project Demo/Sources/front-end/main.js
+++ Projects/Project Demo/Sources/front-end/main.js
@@ -1,26 +1,26 @@
-$(document).ready(function () {
-  // Activate tooltip
-  $('[data-toggle="tooltip"]').tooltip();
+// $(document).ready(function () {
+//   // Activate tooltip
+//   $('[data-toggle="tooltip"]').tooltip();
 
-  // Select/Deselect checkboxes
-  var checkbox = $('table tbody input[type="checkbox"]');
-  $("#selectAll").click(function () {
-    if (this.checked) {
-      checkbox.each(function () {
-        this.checked = true;
-      });
-    } else {
-      checkbox.each(function () {
-        this.checked = false;
-      });
-    }
-  });
-  checkbox.click(function () {
-    if (!this.checked) {
-      $("#selectAll").prop("checked", false);
-    }
-  });
-});
+//   // Select/Deselect checkboxes
+//   var checkbox = $('table tbody input[type="checkbox"]');
+//   $("#selectAll").click(function () {
+//     if (this.checked) {
+//       checkbox.each(function () {
+//         this.checked = true;
+//       });
+//     } else {
+//       checkbox.each(function () {
+//         this.checked = false;
+//       });
+//     }
+//   });
+//   checkbox.click(function () {
+//     if (!this.checked) {
+//       $("#selectAll").prop("checked", false);
+//     }
+//   });
+// });
 
 // create function to update student by call api with axios
 function updateStudent() {
@@ -39,4 +39,18 @@ function updateStudent() {
     .catch(function (error) {
       console.log(error);
     });
+}
+
+function textCallApi() {
+  var jsonResponse = null;
+  axios
+    .get("https://reqres.in/api/users?page=2")
+    .then((res) => {
+      console.log(res);
+      jsonResponse = res;
+    })
+    .catch((err) => console.log(err));
+
+  var data = JSON.parse(JSON.stringify(jsonResponse.data));
+  console.log(data);
 }
