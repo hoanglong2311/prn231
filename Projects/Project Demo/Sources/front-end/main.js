07f6fb6a533625390f91b4c18636d242660eea9c
Hoang Long <longnguyen20011123@gmail.com>
2023-02-15 00:13:31 +0700

    Create function Update

--- Projects/Project Demo/Sources/front-end/main.js
+++ Projects/Project Demo/Sources/front-end/main.js
@@ -1,60 +1,104 @@
-
-console.log('abc');
+console.log("abc");
 
 /**
  * Get list student
  */
 function GetListStudent() {
   $.ajax({
-    url: 'https://localhost:7190/api/student/list',
-    type: 'GET',
-    contentType: 'application/json',
-    success: function(result) {
+    url: "https://localhost:7190/api/student/list",
+    type: "GET",
+    contentType: "application/json",
+    success: function (result) {
       console.log(result);
-      var html = '';
-      $.each(result, function(i, item) {
-        html += '<tr>';
-        html += '<td>' + item.fullName + '</td>';
-        html += '<td>' + item.age + '</td>';
-        html += '<td>' + item.address + '</td>';
-        html += '<td>';
-        html += '<a href="#editStudentModal" class="edit" data-toggle="modal" onclick="editStudent(' + item.id + ')">';
-        html += '<i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>';
-        html += '</a>';
-        html += '<a href="#deleteStudentModal" class="delete" data-toggle="modal" onclick="deleteStudent(' + item.id + ')">';
-        html += '<i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>';
-        html += '</a>';
-        html += '</td>';
-        html += '</tr>';
+      var html = "";
+      $.each(result, function (i, item) {
+        html += "<tr>";
+        html += "<td>" + item.fullName + "</td>";
+        html += "<td>" + item.age + "</td>";
+        html += "<td>" + item.address + "</td>";
+        html += "<td>";
+        html +=
+          '<a href="#editStudentModal" class="edit" data-toggle="modal" onclick="GetStudentById(' +
+          item.id +
+          ')">';
+        html +=
+          '<i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>';
+        html += "</a>";
+        html +=
+          '<a href="#deleteStudentModal" class="delete" data-toggle="modal" onclick="deleteStudent(' +
+          item.id +
+          ')">';
+        html +=
+          '<i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>';
+        html += "</a>";
+        html += "</td>";
+        html += "</tr>";
       });
-      $('#listStudent').html(html);
+      $("#listStudent").html(html);
     },
-    error: function(result, status) {
-      console.log(status,result);
+    error: function (result, status) {
+      console.log(status, result);
     },
-  })
+  });
+
+  window.GetStudentById = function (id) {
+    console.log("get student by id");
+    $.ajax({
+      url: "https://localhost:7190/api/student/id?id=" + id,
+      type: "GET",
+      contentType: "application/json",
+      success: function (result, status, xhr) {
+        $("#studentName").val(result.fullName);
+        $("#studentAge").val(result.age);
+        $("#studentAddress").val(result.address);
+        $("#studentId").val(result.id);
+      },
+    });
+  };
+
+  window.UpdateStudent = function () {
+    console.log("update student");
+    $.ajax({
+      url: "https://localhost:7190/api/student/update",
+      type: "PUT",
+      contentType: "application/json",
+      data: JSON.stringify({
+        id: $("#studentId").val(),
+        fullName: $("#studentName").val(),
+        age: $("#studentAge").val(),
+        address: $("#studentAddress").val(),
+      }),
+      success: function (result, status, xhr) {
+        if (result == 1) {
+          GetAllStudents();
+          $("#editStudentModal").modal("hide");
+          // window.location.reload();
+        }
+      },
+    });
+  };
 }
 
 function addStudent() {
-  console.log('add student');
+  console.log("add student");
   $.ajax({
-    url: 'https://localhost:7190/api/student/save',
-    type: 'POST',
-    contentType: 'application/json',
+    url: "https://localhost:7190/api/student/save",
+    type: "POST",
+    contentType: "application/json",
     data: JSON.stringify({
       fullName: $("#addName").val(),
       age: $("#addAge").val(),
-      address: $("#addAddress").val()
+      address: $("#addAddress").val(),
     }),
-    success: function(result,status, xhr) {
-      if(result == 1){
-        $('#addStudentModal').modal().hide();
+    success: function (result, status, xhr) {
+      if (result == 1) {
+        $("#addStudentModal").modal().hide();
         window.location.reload();
-        console.log('close modal');
+        console.log("close modal");
       }
     },
-    error: function(result,status, xhr) {
-      console.log(status,result);
+    error: function (result, status, xhr) {
+      console.log(status, result);
     },
-  })
+  });
 }
