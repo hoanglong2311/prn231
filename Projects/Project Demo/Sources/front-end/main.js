75a71ca65cd4bb14f042d231331fa25394f21c0f
Dang Huu Quang <handsomeoptics52@gmail.com>
2023-02-14 23:56:27 +0700

    PRN231 - Add Show Students

--- Projects/Project Demo/Sources/front-end/main.js
+++ Projects/Project Demo/Sources/front-end/main.js
@@ -1,5 +1,40 @@
 
 console.log('abc');
+
+/**
+ * Get list student
+ */
+function GetListStudent() {
+  $.ajax({
+    url: 'https://localhost:7190/api/student/list',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(result) {
+      console.log(result);
+      var html = '';
+      $.each(result, function(i, item) {
+        html += '<tr>';
+        html += '<td>' + item.fullName + '</td>';
+        html += '<td>' + item.age + '</td>';
+        html += '<td>' + item.address + '</td>';
+        html += '<td>';
+        html += '<a href="#editStudentModal" class="edit" data-toggle="modal" onclick="editStudent(' + item.id + ')">';
+        html += '<i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>';
+        html += '</a>';
+        html += '<a href="#deleteStudentModal" class="delete" data-toggle="modal" onclick="deleteStudent(' + item.id + ')">';
+        html += '<i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>';
+        html += '</a>';
+        html += '</td>';
+        html += '</tr>';
+      });
+      $('#listStudent').html(html);
+    },
+    error: function(result, status) {
+      console.log(status,result);
+    },
+  })
+}
+
 function addStudent() {
   console.log('add student');
   $.ajax({
